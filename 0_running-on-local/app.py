064e4b914b7b3cae479e4f2dd6a50135047b9982
costin_branzas <costin.branzas@qubiz.com>
2025-04-30 18:11:46 +0300

    basic running of a model

--- 0_running-on-local/app.py
+++ 0_running-on-local/app.py
@@ -1,1 +1,15 @@
-print("hello")
+from flask import Flask
+from transformers import pipeline
+
+
+app = Flask(__name__)
+
+model_path = ""
+classifier = pipeline()
+
+@app.route("/")
+def route1():
+    return "<p>Hello</p>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
